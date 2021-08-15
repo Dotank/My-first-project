76e0b7afef2824df4f40dc22aba69c2e69953c08
Dotank <88946454+Dotank@users.noreply.github.com>
2021-08-15 06:08:42 -0700

    this is my first Lesson

---
+++ targil1.py
@@ -0,0 +1,7 @@
+name=input("enter your name: ")
+age=input ("enter your age: ")
+mail=input("enter your mail: ")
+
+
+print("\nFull name "+ name + "\nYour age is: " + str (age) + "\nyour mail: " + mail)
+
