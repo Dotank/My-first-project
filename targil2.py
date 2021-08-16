06f494bbde021c2b0b7a5d15c699f87e3524f62b
Dotank <88946454+Dotank@users.noreply.github.com>
2021-08-16 07:51:46 -0700

    this is my first Lesson

---
+++ targil2.py
@@ -0,0 +1,21 @@
+num=int(input("Enter a number with 4 digits: "))
+
+'''
+alafim=4
+meot=5
+asarot=6
+ahadot=7
+'''
+# #this is alafim
+# print("alafim=" + str(num//1000))
+#
+# #this is meot
+# print("meot=" + str((num%1000)//100))
+#
+# #This is asarot
+# print("asarot=" + str((num%100)//10))
+#
+# #this is ahadot
+# print("ahadot=" + str(num%10))
+
+print("alafim=" + str(num//1000) + "\nmeot=" + str((num%1000)//100) + "\nasarot=" + str((4567num%100)//10) + "\nahadot=" + str(num%10))
